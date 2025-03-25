4e8b9af1b72cda033aa6821616dc5a32b817a0b2
Harpreet-Kaur-1912 <harpreet.kaur.9012@gmail.com>
2025-03-24 20:22:56 -0400

    Update script.js

--- website/script.js
+++ website/script.js
@@ -1,11 +1,14 @@
-document.getElementById('userForm').addEventListener('submit', async function(e) {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    await fetch('<API_GATEWAY_URL>', {
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    fetch('https://ba32katjdk.execute-api.us-east-1.amazonaws.com/dev', {  // Use your actual API Gateway URL here
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({ name, email })
-    });
-    alert('Submitted successfully!');
+    })
+    .then(response => response.json())
+    .then(data => alert('Registration Successful'))
+    .catch(error => console.error('Error:', error));
 });
